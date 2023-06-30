9b6baac90266bc8e31bd979525e41ff999d37ca1
ReubenNicholls <128763201+ReubenNicholls@users.noreply.github.com>
2023-06-30 12:36:22 +1000

    30/06/23 commit2
    ..

--- Assets/Scripts/EnemySpawnManager.cs
+++ Assets/Scripts/EnemySpawnManager.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class EnemySpawnManager : MonoBehaviour
 {
+    private int startDelay = 2;
+    public float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnEnemy", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    void SpawnEnemy()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(300, 0, 300));
     }
 }
